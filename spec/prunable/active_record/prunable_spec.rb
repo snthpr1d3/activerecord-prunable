7eb5cd7d08e94f331df7e66abd67da73c38d6b29
dr2m <maletin@maletin.work>
2017-10-01 23:18:49 +0300

    add ttl methods

--- spec/prunable/active_record/prunable_spec.rb
+++ spec/prunable/active_record/prunable_spec.rb
@@ -7,6 +7,18 @@ describe ActiveRecord::Prunable do
 
   before do
     allow(subject).to receive(:logger).and_return(Logger)
+
+    if subject.class_variable_defined?(:@@prune_created_after)
+      subject.remove_class_variable(:@@prune_created_after)
+    end
+
+    if subject.class_variable_defined?(:@@prune_updated_after)
+      subject.remove_class_variable(:@@prune_updated_after)
+    end
+
+    if subject.class_variable_defined?(:@@prune_method)
+      subject.remove_class_variable(:@@prune_method)
+    end
   end
 
   describe '.includes' do
@@ -50,67 +62,105 @@ describe ActiveRecord::Prunable do
     let(:incorrect_scope) { ->() { 123 } }
     let(:prunable) { double(is_a?: true, destroy_all: [], delete_all: 0) }
 
-    context 'scope is empty' do
-      it 'raises error when scope result is not ActiveRecord::Relation' do
-        expect(subject.respond_to?(:prunable)).to eq(false)
-        expect_any_instance_of(ActiveRecord::Relation).not_to receive(:present?)
-        expect_any_instance_of(ActiveRecord::Relation).not_to receive(:destroy_all)
-        expect { subject.prune! }.not_to raise_error
+    it 'returns result of .prune_by_method' do
+      prune_result = double(:prune_result).as_null_object
+      allow(subject).to receive(:prunable).and_return(correct_scope)
+      allow(subject).to receive(:prune_by_method).and_return(prune_result)
+      expect(subject.prune!).to eq(prune_result)
+    end
+
+    context 'by prune_created_after method' do
+      let(:scope) { double(:scope).as_null_object }
+      let(:current_time) { Time.parse('2017-10-07 00:00:00') }
+
+      before do
+        subject.prune_created_after(7.days)
+        expect(scope).to receive(:destroy_all)
+      end
+
+      it 'resolves removing scope correctly' do
+        allow(Time).to receive(:current).and_return(current_time)
+
+        expect(subject).to receive(:where)
+          .with('created_at < ?', Time.parse('2017-09-30 00:00:00'))
+          .and_return(scope)
+
+        subject.prune!
+      end
+
+      it 'resolves removing scope correctly with forced time' do
+        expect(subject).to receive(:where)
+          .with('created_at < ?', Time.parse('2017-09-30 00:00:00'))
+          .and_return(scope)
+
+        subject.prune!(current_time: current_time)
       end
     end
 
-    context 'correct scope' do
+    context 'by prune_updated_after method' do
+      let(:scope) { double(:scope).as_null_object }
+      let(:current_time) { Time.parse('2017-10-07 00:00:00') }
+
       before do
-        subject.scope(:prunable, correct_scope)
-        allow(subject).to receive(:defined?).and_return(false)
+        subject.prune_updated_after(7.days)
+        expect(scope).to receive(:destroy_all)
       end
 
-      let(:prune_result) { 0 }
+      it 'resolves removing scope correctly' do
+        allow(Time).to receive(:current).and_return(current_time)
 
-      it 'returns result of .prune_by_method' do
-        allow(subject).to receive(:prune_by_method).and_return(prune_result)
-        allow_any_instance_of(ActiveRecord::Relation).to receive(:present?).and_return(false)
-        expect(subject).to receive(:prune_by_method)
-        expect(subject.prune!).to eq(prune_result)
+        expect(subject).to receive(:where)
+          .with('updated_at < ?', Time.parse('2017-09-30 00:00:00'))
+          .and_return(scope)
+
+        subject.prune!
       end
 
-      context 'prune method has been set' do
-        before do
-          allow(subject).to receive(:prunable).and_return(prunable)
-        end
+      it 'resolves removing scope correctly with forced time' do
+        expect(subject).to receive(:where)
+          .with('updated_at < ?', Time.parse('2017-09-30 00:00:00'))
+          .and_return(scope)
 
-        it 'removes records by destroy_all method' do
-          subject.prune_method(:destroy)
-          expect(prunable).to receive(:destroy_all)
-          subject.prune!
-        end
+        subject.prune!(current_time: current_time)
+      end
+    end
 
-        it 'removes records by delete_all' do
-          subject.prune_method(:delete)
-          expect(prunable).to receive(:delete_all)
-          subject.prune!
-        end
+    context 'by prunable scope' do
+      before do
+        allow(subject).to receive(:prunable).and_return(prunable)
+      end
+
+      it 'resolves removing scope correctly' do
+        expect(prunable).to receive(:destroy_all)
+        subject.prune!
       end
 
       context 'with params' do
-        before do
-          allow(subject).to receive(:prunable).and_return(prunable)
-        end
-
         it 'calls scope with params' do
           expect(subject).to receive(:prunable)
             .with(:foo, :bar)
-            .and_return(:result)
+            .and_return(double.as_null_object)
 
           subject.prune!(:foo, :bar)
         end
       end
     end
 
-    context 'incorrect scope' do
-      it 'returns false' do
-        subject.scope(:prunable, incorrect_scope)
-        expect(subject.prune!).to be false
+    context 'prune method has been set' do
+      before do
+        allow(subject).to receive(:prunable).and_return(prunable)
+      end
+
+      it 'removes records by destroy_all method' do
+        subject.prune_method(:destroy)
+        expect(prunable).to receive(:destroy_all)
+        subject.prune!
+      end
+
+      it 'removes records by delete_all method' do
+        subject.prune_method(:delete)
+        expect(prunable).to receive(:delete_all)
+        subject.prune!
       end
     end
   end
