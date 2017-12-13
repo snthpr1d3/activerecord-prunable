8648a65b50f3fc736920a2461ba551bc746edda4
Ilya Vassilevsky <vassilevsky@gmail.com>
2017-12-13 19:09:38 +0400

    Accumulator goes last :/

--- lib/activerecord-prunable.rb
+++ lib/activerecord-prunable.rb
@@ -14,7 +14,7 @@ module Prunable
     def prune!(*models, prune_method: nil, current_time: nil, params: [])
       models = self.models if models.empty?
 
-      models.each_with_object({}) do |pruned, model|
+      models.each_with_object({}) do |model, pruned|
         pruned[model.table_name] = model.prune!(*params, prune_method: prune_method, current_time: current_time)
       end
     end
